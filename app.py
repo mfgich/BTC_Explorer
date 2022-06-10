acc5cb33e39ed0bcf3ecebb1bb41262d4d5db4aa
mfgich <98882177+mfgich@users.noreply.github.com>
2022-06-10 09:21:25 +0200

    final upload

--- app.py
+++ app.py
@@ -25,24 +25,23 @@ def find():
 
     if request.method == "POST":
 
-        username = str(request.form["block_hash"])
+        hash = str(request.form["block_hash"])
         addr = str(request.form["btc_add"])
 
-        url_hash = "https://blockchain.info/rawblock/"+username
+        url_hash = "https://blockchain.info/rawblock/"+hash
         url_addr = "https://blockchain.info/rawaddr/"+addr
-
-        block = requests.get(url_hash)
-        btc_addr = requests.get(url_addr)
-
-        dict_data = json.loads(block.text)
-        dict_addr = json.loads(btc_addr.text)
-
-        return """
+        if hash == "":
+            pass
+        else:
+            block = requests.get(url_hash)
+            dict_data = json.loads(block.text)
+            return """
                 <body style="background-color: rgb(240, 255, 255);
                 width: 90%;
                 height: auto;
                 margin: auto;">
                 <center>
+                <article id= block>
                 <div>
                 <h1>Blockchain Auszug</h1>
                 <h2>Hash: {0}
@@ -53,24 +52,15 @@ def find():
                 <br>
                 Timestamp: {3}
                 <br>
-                Merkl Root: {4}
+                Merkle Root: {4}
                 <br>
                 Size: {5}
                 <br>
                 Block Nr: {6}
                 <br>
                 </div>
-                <div>
-                <h1>Infos zu BTC Addresse</h1>
-                <h2>Addresse: {7}
-                <br>
-                Eingehend: {8}
-                <br>
-                Ausgehend: {9}
-                <br>
-                Bestand: {10}
-                </div>
                 </center>
+                </article>
                 </body>
                 """.format(
             dict_data.get("hash"),
@@ -79,12 +69,39 @@ def find():
             dict_data.get("time"),
             dict_data.get("mrkl_root"),
             dict_data.get("size"),
-            dict_data.get("block_index"),
+            dict_data.get("block_index")
+            )
+        if addr == "":
+            pass
+        else:
+            btc_addr = requests.get(url_addr)
+            dict_addr = json.loads(btc_addr.text)
+            return """
+                <body style="background-color: rgb(240, 255, 255);
+                width: 90%;
+                height: auto;
+                margin: auto;">
+                <center>
+                <article id = wallet>
+                <div>
+                <h1>Infos zu BTC Addresse</h1>
+                <h2>Addresse: {0}
+                <br>
+                Eingehend: {1}
+                <br>
+                Ausgehend: {2}
+                <br>
+                Bestand: {3}
+                </div>
+                </center>
+                </article>
+                </body>
+                """.format(
             dict_addr.get("address"),
             dict_addr.get("total_received"),
             dict_addr.get("total_sent"),
-            dict_addr.get("final_balance")
-        )
+            dict_addr.get("final_balance")   
+            )
 
     return render_template("index.html")
 
